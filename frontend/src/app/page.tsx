0bf851b414804f90556dce9e7c205b55d572f6c5
PucCris <1565151@sga.pucminas.br>
2024-12-09 20:57:28 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,19 @@
-<div>
-<h1> Welcome to the ConchaYOro App - deploy v2</h1>
-</div>
+name: 'Welcome to the ConchaYOro App - deploy v2'
+description: 'Greet the caller'
+inputs:
+  the-caller:
+    description: 'Who is there'
+    required: true
+    default: 'World'
+outputs:
+  random-number:
+    description: "Random number"
+    value: ${{ steps.random-number-generator.outputs.random-id }}
+runs:
+  using: "composite"
+  steps:
+    - run: echo Hello ${{ inputs.the-caller }}.
+      shell: bash
+    - id: random-number-generator
+      run: echo "::set-output name=random-id::$(echo $RANDOM)"
+      shell: bash
