fcf8c6afeffcadd43d1567f997466cf5ebf7c5ae
PucCris <1565151@sga.pucminas.br>
2024-12-09 21:02:40 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,8 @@
-name: 'Welcome to the ConchaYOro App - deploy v2'
-description: 'Greet the caller'
-inputs:
-  the-caller:
-    description: 'Who is there'
-    required: true
-    default: 'World'
-outputs:
-  random-number:
-    description: "Random number"
-    value: ${{ steps.random-number-generator.outputs.random-id }}
-runs:
-  using: "composite"
-  steps:
-    - run: echo Hello ${{ inputs.the-caller }}.
-      shell: bash
-    - id: random-number-generator
-      run: echo "::set-output name=random-id::$(echo $RANDOM)"
-      shell: bash
+# conchayoro
+Full stack app for tech learning.
+Welcome to our home page!!
+
+<<<<<<< HEAD
+=======
+Welcome to the ConchaYOro App - deploy v2
+>>>>>>> 274f023332b4cf663c76d0cc6fd313c5e80ce971
