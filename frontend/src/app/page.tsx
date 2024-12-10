2d186bc9d031739c98577214e92c9c1abe8d6d3a
PucCris <1565151@sga.pucminas.br>
2024-12-09 21:11:22 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,3 @@
-# conchayoro
-Full stack app for tech learning.
-Welcome to our home page!!
-
-<<<<<<< HEAD
-=======
-Welcome to the ConchaYOro App - deploy v2
->>>>>>> 274f023332b4cf663c76d0cc6fd313c5e80ce971
+<div>
+<h1> Welcome to the ConchaYOro App - deploy v2</h1>
+</div>
